968cba6e4d53bd37a208515983882a8a67972748
mbnbonin <mbnbonin@gmail.com>
2025-02-23 13:28:46 -0300

    Voltando para a versão Conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação esta se conectando ao banco de dados")
-console.log("Testando a aplicação após conectar com o banco de dados")
